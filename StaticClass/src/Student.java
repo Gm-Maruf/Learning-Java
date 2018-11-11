fbf3f841945dc753f704a771adcb918791ce557f
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-11-12 00:06:12 +0600

    Working with Static Class

---
+++ StaticClass/src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	
+	static int number = 0;
+	
+	Student()
+	{
+		number++;
+	}
+	
+	void studentnumber()
+	{
+		System.out.println("Student Number is: "+number);
+	}
+
+}
---
+++ StaticClass/src/studentDemo.java
@@ -0,0 +1,15 @@
+
+public class studentDemo {
+	public static void main(String[] args)
+	{
+		Student s1 = new Student();
+		s1.studentnumber();
+		
+		Student s2 = new Student();
+		s2.studentnumber();
+		
+		Student s3 = new Student();
+		s3.studentnumber();
+	}
+
+}
