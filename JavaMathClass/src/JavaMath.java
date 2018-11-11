ad4bd3a4b4406c067986a2e6420620eb378e01a2
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-11-11 23:54:32 +0600

    woring with MathClass

---
+++ JavaMathClass/src/JavaMath.java
@@ -0,0 +1,22 @@
+
+public class JavaMath {
+	public static void main(String[] args)
+	{
+		int x = 2;
+		int y = 3;
+		int max = Math.max(x, y);
+		
+		System.out.println("Maximum number is: "+max);
+		
+		int absolute = Math.abs(y);
+		System.out.println("Absolute value of y is: "+absolute);
+		
+		double powr = Math.pow(x, y);
+		System.out.println("x to the power y is: "+powr);
+		
+		int round = Math.round(9.8f);
+		System.out.println("Round number of 9.8 is: "+round);
+		 
+	}
+
+}
