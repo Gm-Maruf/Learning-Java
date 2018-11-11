a3231a9fbe71279fa13e3253c3ecb8c2094bddaa
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-11-11 23:49:17 +0600

    Factorial Number implements using recursion

---
+++ Factorial/src/FactorialUsingRecursion.java
@@ -0,0 +1,15 @@
+
+public class FactorialUsingRecursion {
+	int fact(int n)
+	{
+		if(n == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return n*fact(n-1);
+		}
+	}
+
+}
---
+++ Factorial/src/TestFactorial.java
@@ -0,0 +1,11 @@
+
+public class TestFactorial {
+	public static void main(String[] args)
+	{
+		FactorialUsingRecursion ob = new FactorialUsingRecursion();
+		int ans = ob.fact(4);
+		
+		System.out.println("The factorial value is: "+ans);
+	}
+
+}
