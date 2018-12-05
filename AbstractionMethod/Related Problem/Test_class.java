4fa8c9fb0db4ecd8a7801fd4161ce51c391e6abe
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-12-05 23:57:56 +0600

    Test class for Shapes

---
+++ AbstractionMethod/Related Problem/Test_class.java
@@ -0,0 +1,18 @@
+package problem_solving;
+
+public class Test_class {
+	public static void main(String[] srgs)
+	{
+		Shapes shape;
+		 
+		 shape = new Rectangle(10,20);
+		 shape.area();
+		 
+		 shape = new Triangle(10,20);
+		 shape.area(); 
+		 
+		 shape = new Circle(10);
+		 shape.area();
+	}
+
+}
