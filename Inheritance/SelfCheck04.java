d46d87f4392eba629133139d3b4b84cd90491706
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-12-10 01:20:36 +0600

    Chapter 9

---
+++ Inheritance/SelfCheck04.java
@@ -0,0 +1,25 @@
+package chapter9;
+
+import javax.swing.JFrame;
+
+public class SelfCheck04 {
+	public SelfCheck04(String title) {
+	      super();
+	      setBounds(0,0,600,400);
+	     // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   }
+	   
+	private void setBounds(int i, int j, int k, int l) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		   SelfCheck04 bw = new SelfCheck04("My Window");
+	      bw.setVisible(true);
+	   }
+	private void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
